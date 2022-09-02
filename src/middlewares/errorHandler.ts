d119ed8b76be7508542ffdcd1b8817f7805b7874
Lucas Lafèré <laferelucas@gmail.com>
2022-09-02 12:45:42 -0300

    better error handling

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 
 export default function errorHandler (error: any, req: Request, res: Response, next: NextFunction) {
     console.log(error);
-    if (error.type) {
-      return res.sendStatus(error.message);
+    if (error.type === 'wrong-type') {
+      return res.status(400).send(error.message);
     }
   
     res.sendStatus(500);
