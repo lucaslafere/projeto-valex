93e56e5a21339d8474f4bf8ac4ba7a609fc3f912
Lucas Lafèré <laferelucas@gmail.com>
2022-09-02 12:21:54 -0300

    initialized project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import "express-async-errors";
+import errorHandler from "./middlewares/errorHandler";
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+// app.use(router)
+app.use(errorHandler);
+
+
+const PORT = Number(process.env.PORT);
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+
+
